6581c893578c377dd44511c7fd87c1a5f9d16c84
ledonosoc <l.e.donoso.c@gmail.com>
2022-05-22 03:21:28 -0400

    Update API_blocked.py
    Correcciones de sintaxis

--- API_blocked.py
+++ API_blocked.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import json
 
 # create the Flask app
 app = Flask(__name__)
@@ -8,7 +9,7 @@ def GetBlocked(blocklist):
     bloqueadosactuales = json.dump({})
     for user in bloqueados:
         if time.time()-bloqueados[user]<60:
-            bloqueadosactuales.update(user)
+            bloqueadosactuales.update(json.dump({user:bloqueados[user]}))
     with open('BlockedUsers.json', 'w') as file:
         json.dump(bloqueadosactuales, file, indent=4)
     return bloqueadosactuales
