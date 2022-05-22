44f2831a48de1e46435284082eb7e4f173e010e9
ledonosoc <l.e.donoso.c@gmail.com>
2022-05-22 03:15:23 -0400

    Implementacion API_blocked
    Se mezcló el código de Consumer y Producer en blockuser

---
+++ API_blocked.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+def GetBlocked(blocklist):
+    bloqueados = json.loads(blocklist)
+    bloqueadosactuales = json.dump({})
+    for user in bloqueados:
+        if time.time()-bloqueados[user]<60:
+            bloqueadosactuales.update(user)
+    with open('BlockedUsers.json', 'w') as file:
+        json.dump(bloqueadosactuales, file, indent=4)
+    return bloqueadosactuales
+
+@app.route('/blocked')
+def blocked():
+    blocklist = open('BlockedUsers.json','r')
+    return GetBlocked(blocklist)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
